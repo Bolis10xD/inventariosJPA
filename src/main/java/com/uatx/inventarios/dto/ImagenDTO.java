368b8dc84f999735949036f52026fab2c2fcd0d7
Oliver Rodriguez <oliverhiito@gmail.com>
2020-03-28 13:18:01 -0600

    Creación de la clase ImagenDTO con sus atributos

---
+++ src/main/java/com/uatx/inventarios/dto/ImagenDTO.java
@@ -0,0 +1,36 @@
+package com.uatx.inventarios.dto;
+
+import javax.persistence.Column;
+
+public class ImagenDTO {
+    private String nombre;
+
+    private String mimeType;
+
+    private String dataBase64;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public String getDataBase64() {
+        return dataBase64;
+    }
+
+    public void setDataBase64(String dataBase64) {
+        this.dataBase64 = dataBase64;
+    }
+}
