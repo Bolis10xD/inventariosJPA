d49a5e2718aed91368ceadaa99cf2bf4c1d39728
Oliver Rodriguez <oliverhiito@gmail.com>
2020-03-28 13:17:37 -0600

    Se añadio el atributo imagendto a la clase ProductoDTO

--- src/main/java/com/uatx/inventarios/dto/ProductoDTO.java
+++ src/main/java/com/uatx/inventarios/dto/ProductoDTO.java
@@ -3,6 +3,8 @@ package com.uatx.inventarios.dto;
 public class ProductoDTO {
     private String nombre;
 
+    ImagenDTO imagendto;
+
     public String getNombre() {
         return nombre;
     }
@@ -10,4 +12,8 @@ public class ProductoDTO {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public ImagenDTO getImagendto() { return imagendto; }
+
+    public void setImagendto(ImagenDTO imagendto) { this.imagendto = imagendto; }
 }
